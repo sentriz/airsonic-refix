53e10186be9bd0b607181bc09b27d81916b4c708
sentriz <senan@senan.xyz>
2023-10-16 23:43:18 +0100

    AlbumDetails: add album duration

--- src/shared/api.ts
+++ src/shared/api.ts
@@ -41,6 +41,7 @@ export interface Album {
   image?: string
   lastFmUrl?: string
   musicBrainzUrl?: string
+  duration: number
   tracks?: Track[]
 }
 
@@ -570,6 +571,7 @@ export class API {
       musicBrainzUrl: item.musicBrainzId
         ? `https://musicbrainz.org/release/${item.musicBrainzId}`
         : undefined,
+      duration: item.duration,
       tracks: (item.song || []).map(this.normalizeTrack, this)
     }
   }
