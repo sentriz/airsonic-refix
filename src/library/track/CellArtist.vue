3e84631aa5853707568b9b7182728789a6de9f2d
sentriz <senan@senan.xyz>
2024-07-19 23:05:15 +0100

    AlbumDetails,CellArtist: fix artist spacing

--- src/library/track/CellArtist.vue
+++ src/library/track/CellArtist.vue
@@ -2,12 +2,8 @@
   <td class="d-none d-lg-table-cell">
     <template v-for="(artist, index) in props.track.artists">
       <span v-if="index > 0" :key="artist.id">, </span>
-      <router-link v-if="artist.id" :key="`${artist.id}-link`" :to="{name: 'artist', params: { id: artist.id }}" @click.native.stop>
-        {{ artist.name }}
-      </router-link>
-      <template v-else>
-        {{ artist.name }}
-      </template>
+      <router-link v-if="artist.id" :key="`${artist.id}-link`" :to="{name: 'artist', params: { id: artist.id }}" @click.native.stop>{{ artist.name }}</router-link>
+      <template v-else>{{ artist.name }}</template>
     </template>
   </td>
 </template>
