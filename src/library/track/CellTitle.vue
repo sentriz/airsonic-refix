aadcf0fb849cf13d6f81eb7383f2d854a9323b7c
sentriz <senan@senan.xyz>
2026-05-14 17:49:56 +0100

    (to upstream) Make contributor names clickable in CellTitle

--- src/library/track/CellTitle.vue
+++ src/library/track/CellTitle.vue
@@ -1,6 +1,14 @@
 <template>
   <td>
-    <slot>{{ track.title }}</slot>
+    <slot>
+      <template v-for="(part, i) in titleParts">
+        <router-link v-if="part.value?.artist?.id" :key="i"
+                     :to="{name: 'artist', params: { id: part.value.artist.id }}"
+                     class="contributor-link"
+                     @click.native.stop>{{ part.text }}</router-link>
+        <template v-else>{{ part.text }}</template>
+      </template>
+    </slot>
     <div v-if="track.description" class="text-muted">
       <small>{{ track.description }}</small>
     </div>
@@ -10,15 +18,27 @@
   </td>
 </template>
 <script lang="ts">
-  import { defineComponent } from 'vue'
-  import { formatArtists } from '@/shared/utils'
+  import { defineComponent, PropType } from 'vue'
+  import { formatArtists, splitByLabels } from '@/shared/utils'
+  import { Track } from '@/shared/api'
 
   export default defineComponent({
     props: {
-      track: { type: Object, required: true }
+      track: { type: Object as PropType<Track>, required: true }
+    },
+    computed: {
+      titleParts() {
+        return splitByLabels(this.track.title, this.track.contributors || [], c => c.artist?.name || '')
+      }
     },
     methods: {
       formatArtists
     }
   })
 </script>
+<style scoped>
+  .contributor-link {
+    text-decoration: underline;
+    text-decoration-color: var(--bs-secondary-color);
+  }
+</style>
