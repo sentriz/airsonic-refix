dc29eee7ed3f1aa6830c4e11b77c76420a1f50fb
sentriz <senan@senan.xyz>
2026-05-14 17:23:26 +0100

    (to upstream) Add Album.displayArtist from server

--- src/shared/api.ts
+++ src/shared/api.ts
@@ -41,6 +41,7 @@ export interface Album {
   name: string
   description?: string
   artists: {name: string, id: string}[]
+  displayArtist?: string
   year: number
   favourite: boolean
   genres: Genre[]
@@ -590,6 +591,7 @@ export class API {
       artists: item.artists?.length
         ? item.artists
         : [{ id: item.artistId, name: item.artist }],
+      displayArtist: item.displayArtist,
       image: this.getCoverArtUrl(item),
       year: item.year || 0,
       favourite: !!item.starred,
